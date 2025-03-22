cd09b1cd1e84c7940e384c6fba6ef71620970075
devenjarvis <devenjarvis@gmail.com>
2025-03-21 21:14:32 -0400

    chore: Add validate to env file loading

--- internal/core/envfile.go
+++ internal/core/envfile.go
@@ -5,12 +5,44 @@ import (
 	"strings"
 )
 
+var ErrInvalidEnvFile = os.ErrInvalid
+
+func validateEnvFile(envVars []string) (err error) {
+	// Valiate the env file is in the correct format
+	for _, envVar := range envVars {
+		parts := strings.SplitN(envVar, "=", 2)
+		// If there is not exactly one equals sign, raise an error
+		if len(parts) != 2 {
+			err = ErrInvalidEnvFile
+			return
+		}
+		// If the key is empty, raise an error
+		if strings.TrimSpace(parts[0]) == "" {
+			err = ErrInvalidEnvFile
+			return
+		}
+		// If the value is empty, raise an error
+		if strings.TrimSpace(parts[1]) == "" {
+			err = ErrInvalidEnvFile
+			return
+		}
+	}
+
+	return
+}
+
 func ReadEnvFile(path string) (envVars []string, err error) {
 	envs, err := os.ReadFile(path)
 	if err != nil {
 		return
 	}
 	envVars = strings.Split(string(envs), "\n")
+
+	err = validateEnvFile(envVars)
+	if err != nil {
+		envVars = []string{}
+		return
+	}
 
 	return
 }
