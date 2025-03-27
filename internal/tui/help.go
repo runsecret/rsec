b5fedf1111ff78385ddfee707890f5a74e21bd93
devenjarvis <devenjarvis@gmail.com>
2025-03-27 19:58:15 -0400

    chore: Replace clipboard library with go-osc52 for copying secrets

--- internal/tui/help.go
+++ internal/tui/help.go
@@ -2,8 +2,6 @@ package tui
 
 import "github.com/charmbracelet/glamour"
 
-var renderer *glamour.TermRenderer
-
 func RenderMkDown(in string) string {
 	r, _ := glamour.NewTermRenderer(
 		// detect background color and pick either the default dark or light theme
