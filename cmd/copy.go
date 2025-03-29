26a2c50b8e2db0a35d0593ffc760151fa56618c7
devenjarvis <devenjarvis@gmail.com>
2025-03-29 06:55:52 -0400

    chore: standardize cmd format

--- cmd/copy.go
+++ cmd/copy.go
@@ -17,28 +17,26 @@ func NewCopyCmd() *cobra.Command {
 		Long:  `Copy the secret value from a reference into your clipboard`,
 		Example: `  rsec copy my-secret
   rsec get my-secret --out secret.txt`,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		Run: func(cmd *cobra.Command, args []string) {
 			secretRef := args[0]
 			vaultClient := secrets.NewVaultClient()
 
 			// Retrieve secret if it exists
 			secret, err := vaultClient.CheckForSecret(secretRef)
 			if err != nil {
-				return err
+				fmt.Println("X - Error retrieving secret: ", err)
 			}
 
 			// Write to clipboard using OSC 52
 			if secret != "" {
 				_, err = osc52.New(secret).WriteTo(os.Stderr)
 				if err != nil {
-					return err
+					fmt.Println("X - Error writing to clipboard: ", err)
 				}
 
 				fmt.Println("✓ - Secret copied to clipboard!")
-				return nil
 			}
 			fmt.Println("X - No secret found! Double check the secret identifier provided?")
-			return nil
 		},
 	}
 }
