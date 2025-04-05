ebe1cc7dc6dccdbca3e0b29085bf11be11ea9ad3
devenjarvis <devenjarvis@gmail.com>
2025-04-05 14:28:04 -0400

    feat: Add localstack-dependent test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -5,8 +5,32 @@ import (
 	"io"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestCopyCommand(t *testing.T) {
+	require := require.New(t)
+	assert := assert.New(t)
+	cmd := NewCopyCmd()
+
+	// Capture output
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+
+	// Set up command arguments
+	cmd.SetArgs([]string{"aws://us-east-1/000000000000/test/api/keys"})
+
+	// Execute command
+	err := cmd.Execute()
+	// Expect no error
+	require.NoError(err)
+
+	// Ensure output is as expected
+	out, err := io.ReadAll(b)
+	require.NoError(err)
+	assert.Equal(string(out), "✓ - Secret copied to clipboard!\n")
+}
 
 func TestCopyCommand_MissingArgument(t *testing.T) {
 	require := require.New(t)
